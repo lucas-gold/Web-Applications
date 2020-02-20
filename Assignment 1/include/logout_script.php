010e7d2bb596736b9cc5360b9977e121900ae23f
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-02-19 23:37:46 -0500

    Update logout_script.php

--- Assignment 1/include/logout_script.php
+++ Assignment 1/include/logout_script.php
@@ -1,9 +1,9 @@
 <?php
   session_start();
   if (!isset($_SESSION['username'])) {
-    header("Location: ../test.php");
+    header("Location: ../index.php");
   } else {
     session_destroy();
-    header("Location: ../test.php");
+    header("Location: ../index.php");
   }
  ?>
