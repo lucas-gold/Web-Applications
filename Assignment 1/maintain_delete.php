74b3579475702acc62944875b9e7822eee692d64
tc-proj <60370621+tc-proj@users.noreply.github.com>
2020-02-28 08:04:43 -0500

    Add files via upload

--- Assignment 1/maintain_delete.php
+++ Assignment 1/maintain_delete.php
@@ -14,7 +14,7 @@
     echo "try again";
     }
     */
-    require_once 'sql/connection.php';
+    require_once 'include/connection.php';
     include 'include/navigation.php'
 ?>
 
@@ -55,7 +55,7 @@ $(document).ready(function(){
         if(form == "plans"){
             $("#form_plans").toggle();
         }
-        if(form == "attraction_photos"){
+        if(form == "country"){
             $("#form_attraction_photos").toggle();
         }
         if(form == "invoice"){
@@ -64,12 +64,18 @@ $(document).ready(function(){
         if(form == "reviews"){
             $("#form_reviews").toggle();
         }
+        if(form == "country"){
+            $("#form_country").toggle();
+        }
+        if(form == "continent"){
+            $("#form_continent").toggle();
+        }
     });
 });
 </script>
 
 <body>
-    <?php require_once 'sql/delete.php';?>
+    <?php require_once 'travel_planner/delete.php';?>
 
     <!--Create success/error message-->
     <?php
@@ -84,23 +90,24 @@ $(document).ready(function(){
     <?php endif?>
 
     <div class="container main">
-         <h2>MaintainDB [Delete]</h2> 
-         <form name="chooseTable" id="chooseTable">
-         <div class="form-group"> 
-            <label for="selectTable">Choose a table:</label>
-            <select class="form-control" name="selectTable" id="selectTable">
-                <option value="users" selected>users</option>
-                <option value="attractions">attractions</option>
-                <option value="plans">plans</option>
-                <option value="attraction_photos">attraction_photos</option>
-                <option value="invoice">invoice</option>
-                <option value="reviews">reviews</option>
-            </select>
-        </div>
+        <h2>MaintainDB [Delete]</h2> 
+        <form name="chooseTable" id="chooseTable">
+            <div class="form-group"> 
+                <label for="selectTable">Choose a table:</label>
+                <select class="form-control" name="selectTable" id="selectTable">
+                    <option value="users" selected>users</option>
+                    <option value="attractions">attractions</option>
+                    <option value="plans">plans</option>
+                    <option value="country">country</option>
+                    <option value="continent">continent</option>
+                    <option value="invoice">invoice</option>
+                    <option value="reviews">reviews</option>
+                </select>
+            </div>
         </form>
         <button id="btn_selectTable" class="btn btn-success">Select</button>
 
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_users">
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_users">
             <div class="form-group">
                 <label for="userid">User ID</label>
                 <input type="number" class="form-control" name="userid" id="userid" maxlenght="10" required>
@@ -108,119 +115,52 @@ $(document).ready(function(){
             <button type="submit" name="btn_delete_user" class="btn btn-danger" value="submit" value="submit">Delete</button>
         </form>
 
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_plans">
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_plans">
             <div class="form-group">
-                <label for="startDate">Start Date</label><br>
-                <small><em>YYYY-MM-DD</em></small>
-                <input type="text" class="form-control" id="startDate" name="startDate" pattern="([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))" required>
+                <label for="planId">Plan ID</label><br>
+                <input type="number" class="form-control" id="planId" name="planId" maxlength="6" required>
             </div>
-            <div class="form-group">
-                <label for="duration">Duration</label>
-                <input type="text" class="form-control" id="duration" name="duration" required>
-            </div>
-            <div class="form-group">
-                <label for="attraction_id1">1st Attraction ID</label>
-                <input type="number" class="form-control" id="attraction_id1" name="attraction_id1" maxlength="6" required>
-            </div>
-            <div class="form-group">
-                <label for="attraction_id2">2nd Attraction ID</label>
-                <small><em>*optional</em></small>
-                <input type="number" class="form-control" maxlength="6" id="attraction_id2" name="attraction_id2">
-            </div>
-            <div class="form-group">
-                <label for="attraction_id3">3rd Attraction ID</label>
-                <small><em>*optional</em></small>
-                <input type="number" class="form-control" maxlength="6" id="attraction_id3" name="attraction_id3">
-            </div>
-            <div class="form-group">
-                <label for="transitFare">Transit Fare</label>
-                <input type="number" class="form-control" id="transitFare" name="transitFare" step="0.05" pattern="^[0-9]+(\.[0-9]{1,2})?$" required>
-            </div>
-            <div class="form-group">
-                <label for="price">Price</label>
-                <input type="number" class="form-control" id="price" name="price" pattern="^[0-9]+(\.[0-9]{1,2})?$" required>
-            </div>
-            <button type="submit" name="btn_delete_plan" class="btn btn-success" value="submit">delete</button>
+            <button type="submit" name="btn_delete_plan" class="btn btn-danger" value="submit">Delete</button>
         </form>
 
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_attractions">
-            <div class="form-group">
-                <label for="attraction">Attraction Name</label>
-                <input type="text" class="form-control" id="attraction" required>
-            </div>
-            <div class="form-group">
-                <label for="continent">Continent</label>
-                <input type="text" class="form-control" id="continent" required>
-            </div>
-            <div class="form-group">
-                <label for="country">Country</label>
-                <input type="text" class="form-control" id="country" required>
-            </div>
-            <div class="form-group">
-                <label for="dateCreated">Date Created</label>
-                <input type="text" class="form-control" id="dateCreated" required>
-            </div>
-            <div class="form-group">
-                <label for="founder">Founder</label>
-                <input type="text" class="form-control" id="founder" required>
-            </div>
-            <button type="submit" name="btn_delete_attraction" class="btn btn-success" value="submit">delete</button>
-        </form>
-
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_attraction_photos">
-            <div class="form-group">
-                <label for="filePath">File Path</label>
-                <input type="text" class="form-control" id="filePath" required>
-            </div>
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_attractions">
             <div class="form-group">
                 <label for="attractionId">Attraction ID</label>
-                <input type="text" class="form-control" id="attractionId" required>
+                <input type="number" class="form-control" name="attractionId" id="attractionId"  maxlength="6" required>
             </div>
-            <button type="submit" name="btn_delete_photo" class="btn btn-success" value="submit">delete</button>
+            <button type="submit" name="btn_delete_attraction" class="btn btn-danger" value="submit">delete</button>
         </form>
 
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_invoice">
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_invoice">
             <div class="form-group">
-                <label for="planId">Plan ID</label>
-                <input type="text" class="form-control" id="planId" required>
+                <label for="invoiceID">Invoice ID</label>
+                <input type="number" class="form-control" name="invoiceID" id="invoiceID" maxlenght="6" required>
             </div>
-            <div class="form-group">
-                <label for="username">username</label>
-                <input type="text" class="form-control" name="username" id="username" maxlenght="30" required>
-            </div>
-            <div class="form-group">
-                <label for="numAdults">Adults</label>
-                <input type="number" class="form-control" id="numAdults" name="numAdults" min="1" max="100" required>
-            </div>
-            <div class="form-group">
-                <label for="numChild">Children</label>
-                <input type="number" class="form-control" id="numChild" name="numChild" min="1" max="100" required>
-            </div>
-            <button type="submit" name="btn_delete_invoice" class="btn btn-success" value="submit">delete</button>
+            <button type="submit" name="btn_delete_invoice" class="btn btn-danger" value="submit">delete</button>
         </form>
 
-        <form action="sql/delete.php" method="POST" class="form_delete" id="form_reviews">
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_country">
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" name="username" id="username" maxlenght="30" required>
+                <label for="countryId">Country ID</label>
+                <input type="number" class="form-control" name="countryId" id="countryId" maxlenght="6" required>
             </div>
+            <button type="submit" name="btn_insert_country" class="btn btn-danger" value="submit">Insert</button>
+        </form>
+
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_continent">
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" name="username" id="username" maxlenght="30" required>
+                <label for="continentId">Continent ID</label>
+                <input type="number" class="form-control" name="continentId" id="continentId" max="6" required>
             </div>
+            <button type="submit" name="btn_insert_continent" class="btn btn-danger" value="submit">Insert</button>
+        </form>
+
+        <form action="travel_planner/delete.php" method="POST" class="form_delete" id="form_reviews">
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" name="username" id="username" maxlenght="30" required>
+                <label for="reviewId">Review ID</label>
+                <input type="number" class="form-control" name="reviewId" id="reviewId" maxlenght="6" required>
             </div>
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" name="username" id="username" maxlenght="30" required>
-            </div>
-            <div class="form-group">
-                <label for="review">Review</label>
-                <textarea class="form-control" id="review" rows="4" required></textarea>
-            </div>
-            <button type="submit" name="btn_delete_review" class="btn btn-success" value="submit">delete</button>
+            <button type="submit" name="btn_delete_review" class="btn btn-danger" value="submit">delete</button>
         </form>
     </div>
 </body>
