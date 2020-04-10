a65bc4b92aa9a26b93598f5503e9e9a4d730e049
lucas-gold <37881053+lucas-gold@users.noreply.github.com>
2020-04-10 16:51:19 -0400

    Add files via upload
    added nlp, needs formatting

---
+++ A3/searchdb.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<?php
+$q = $_GET['search'];
+
+$conn = mysqli_connect('localhost','root','','travel_planner');
+if (!$conn) {
+  die('Could not connect: ' . mysqli_error($conn));
+}
+
+mysqli_select_db($conn,"travel_planner");
+
+?>
+<body>
+  <?php
+  $chars = "0123456789";
+  $q_arr = str_word_count( $q , 1, $chars);
+  $q_count = str_word_count( $q , 0, $chars);
+
+  //place, continent, country, city, keyword(s), min-price, max-price
+  $q_parsed = array("Attraction" => "notset", "Continent" => "notset", "Country" => "notset", "City" => "notset", "Keywords" => "notset", "min-price" => "notset", "max-price" => "notset");
+  $prev_word = ""; //for multi word consideration i.e. New Zealand
+  $tables = array("Continent", "Country", "Attraction");
+
+  for ($x = 0; $x < $q_count; $x++)
+  {
+    $word = $q_arr[$x];
+    echo $q_arr[$x];
+
+    //STEM WORDS:
+    //Remove trailing S
+    if (substr($word, -3) == "ies") {
+      $word = substr($word, 0, -3)."y";
+    }
+    else if (substr($word, -1) == "s") {
+      $word = substr($word, 0, -1);
+    }
+    //Change trailing "ian" to "a"
+    if (substr($word, -3) == "ian") {
+      $word = substr($word, 0, -3)."a";
+      echo $word;
+    }
+    //**********end of stem words***************
+
+
+    if ($x < $q_count-1) {
+      $next_word = $q_arr[$x+1];
+
+      //SET PRICE
+      //look for current word of "under" "over" or "for"
+      //if next word exists, find number
+      if (substr($next_word, 0, 1) == "$") {
+        $next_word = substr($next_word, 1);
+      }
+      if (is_numeric($next_word)) {
+        if ($word == "over")
+        {
+          $q_parsed["min-price"] = $next_word;
+        }
+        else if ($word == "under")
+        {
+          $q_parsed["max-price"] = $next_word;
+        }
+        else if ($word == "for")
+        {
+          $q_parsed["min-price"] = $next_word;
+          $q_parsed["max-price"] = $next_word;
+        }
+      }
+      //************end of set price******************
+
+    }
+
+    //CHECK MULTI WORDS
+    $single_word = $word;
+    if ($prev_word != "") {
+      //only consider closest previous word
+      if (str_word_count($prev_word , 0, $chars) > 1) {
+        $prev_word_start = strrpos( $prev_word , " ") + 1;
+        $prev_word = substr($prev_word, $prev_word_start);
+      }
+      $word = $prev_word." ".$word;
+    }
+
+//    echo "(~".$word."~)";
+    //search for query term(s) in each table
+    for ($i = 0; $i < count($tables); $i++) {
+      $sql = "SELECT name FROM $tables[$i] WHERE name = '$word'";
+      $result = mysqli_query($conn,$sql);
+      if (mysqli_num_rows($result) == 0) {
+        $prev_word = $word;
+      }
+      else {
+        while($row = mysqli_fetch_array($result)) {
+          //do something
+          $q_parsed[$tables[$i]] = $row["name"];
+          //$prev_word = ""; //does this do anything?
+        }
+      }
+
+      //repeat with single word
+      //in case pair of words do not go together
+      $sql = "SELECT name FROM $tables[$i] WHERE name = '$single_word'";
+      $result = mysqli_query($conn,$sql);
+      if (mysqli_num_rows($result) == 0) {
+        $prev_word = $word;
+      }
+      else {
+        while($row = mysqli_fetch_array($result)) {
+          //do something
+          $q_parsed[$tables[$i]] = $row["name"];
+          //$prev_word = ""; //no effect?
+        }
+      }
+    } //end of inner for loop
+/*
+    echo "...";
+    echo $q_parsed["Country"];
+    echo ", ";
+    echo $q_parsed["Continent"];
+    echo ", ";
+    echo $q_parsed["Attraction"];
+    echo "!!! ";
+*/
+
+    //CHECK FOR MATCHING KEYWORDS (type) IN Attraction
+    $sql = "SELECT type FROM Attraction WHERE type = '$single_word'";
+    $result = mysqli_query($conn,$sql);
+    while($row = mysqli_fetch_array($result)) {
+      //do something
+      $q_parsed["Keywords"] = $row["type"];
+      //$prev_word = ""; //no effect?
+    }
+
+    echo "((".$q_parsed["Keywords"] ."))";
+
+
+  } //end of for loop to get parsed text
+
+  //GET SEARCH RESULTS BASED ON PARSED TEXT
+  $name_input = $q_parsed['Attraction'];
+  $country_input = $q_parsed['Country'];
+  $city_input = $q_parsed['City'];
+  $keyword_input = $q_parsed['Keywords'];
+
+  $sql = "SELECT name, type, location, country FROM Attraction WHERE
+  ('$name_input' = 'notset' OR  name = '$name_input')
+  AND
+  ('$country_input' = 'notset' OR  country = '$country_input')
+  AND
+  ('$city_input' = 'notset' OR location = '$city_input')
+  AND
+  ('$keyword_input' = 'notset' OR type = '$keyword_input')
+  AND
+  NOT ('$keyword_input' = 'notset' AND '$city_input' = 'notset' AND '$country_input' = 'notset' AND '$name_input' = 'notset')
+  ";
+
+
+  $result = mysqli_query($conn,$sql);
+  while($row = mysqli_fetch_array($result)) {
+    echo $row[0];
+    //format search results here
+  }
+
+mysqli_close($conn);
+?>
+</body>
+</html>
