226e6071913b60df1e962594ea4fb189c26665fe
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-04-16 15:19:12 -0400

    Update register.php

--- A3/register.php
+++ A3/register.php
@@ -17,10 +17,6 @@
   	  <input type="text" name="username" value="<?php echo $username; ?>">
   	</div>
   	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
   	  <label>Password</label>
   	  <input type="password" name="password_1">
   	</div>
