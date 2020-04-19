60b3fd74498c1285af788a73996a8ccb165c81e3
Mustafa <38087418+Thames18@users.noreply.github.com>
2020-04-18 23:43:56 -0400

    Add files via upload

--- A3/index.php
+++ A3/index.php
@@ -72,16 +72,10 @@ if (!isset($_SESSION['username'])) {
 
         <!-- logged in user information -->
         <?php  if (isset($_SESSION['username'])) : ?>
-          <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-          <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+          <p>&emsp; Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+          <p> <a href="index.php?logout='1'" style="color: red;"> &emsp; logout</a> </p>
         <?php endif ?>
     </div>
-
-
-
-
-
-
 
   <script>
     var myApp = angular.module("travelApp", ['ngSanitize', 'ngRoute']);
