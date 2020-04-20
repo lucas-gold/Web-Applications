40a596cac228132ec598a0c2e84d7bf76cde3bf3
lucas-gold <37881053+lucas-gold@users.noreply.github.com>
2020-04-20 16:13:55 -0400

    Update get_compare.php

--- A3/travel_planner/get_compare.php
+++ A3/travel_planner/get_compare.php
@@ -4,6 +4,7 @@
 
 <?php
 $q = strval($_GET['q']);
+$originalid = strval($_GET['original']);
 
 $conn = mysqli_connect('localhost','root','','travel_planner');
 if (!$conn) {
@@ -12,13 +13,22 @@ if (!$conn) {
 
 mysqli_select_db($conn,"travel_planner");
 
-$close_id = 0;
+
+$sql="SELECT lat,lon,name FROM Attraction WHERE id = '".$originalid."'";
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($result)) {
+  $lat1 = $row["lat"];
+  $lon1 = $row["lon"];
+  $name1 = $row["name"];
+}
 
 
 $sql="SELECT * FROM Attraction WHERE id = '".$q."'";
 
 $result = mysqli_query($conn,$sql);
 while($row = mysqli_fetch_array($result)) {
+  $lat2 = $row["lat"];
+  $lon2 = $row["lon"];
 
   echo "<table style='margin-top: -8%;'><tr><td valign='top'>";
   echo "<img class = 's1' src = img/" . $row["picture2"] . "></img></td><td>";
@@ -26,8 +36,35 @@ while($row = mysqli_fetch_array($result)) {
   echo $row["city"].", ".$row["country"];
   echo "</h3><br>";
   echo $row["description"]." <span style='font-style:italic;'>Rated ".$row["rating"]."/5.</span><br>";
-  echo " Cost: $".$row["price"];
-  echo " - <a href='' style='font-style:italic;'> Add to cart.</a><br>";
+  //echo " Cost: $".$row["price"];
+  echo "<a onclick='hide_compare()' style='font-style:italic;'> Click here to purchase for $".$row["price"].".</a><br>";
+
+  //distance function taken from https://www.geodatasource.com/developers/php
+  function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+    if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+      return 0;
+    }
+    else {
+      $theta = $lon1 - $lon2;
+      $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+      $dist = acos($dist);
+      $dist = rad2deg($dist);
+      $miles = $dist * 60 * 1.1515;
+      $unit = strtoupper($unit);
+
+      if ($unit == "K") {
+        return round(($miles * 1.609344),0);
+      } else if ($unit == "N") {
+        return round(($miles * 0.8684),0);
+      } else {
+        return round($miles,0);
+      }
+    }
+  }
+
+  echo "<div style='font-weight:bold;'>".distance($lat1, $lon1, $lat2, $lon2, "K") . " Kilometres from ".$name1."</div>";
+
+  //echo " - <a onclick='hide_compare()' style='font-style:italic;'> Add to cart.</a><br>";
   echo "</tr></table>";
 }
 
